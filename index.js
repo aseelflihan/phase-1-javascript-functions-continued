38a3df8c515dc96b09dfdb612aeadcc4f76f6176
aseelflihan <aseelflihan@gmail.com>
2022-12-23 22:34:47 +0300

    complated assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+function saturdayFun(activity ) {
+    if (activity === 'bathe my dog') {
+      return `This Saturday, I want to bathe my dog!`;
+    }
+    return `This Saturday, I want to roller-skate!`;
+  }
+
+
+  function mondayWork(activity  ) {
+    if (activity === 'work from home') {
+        return "This Monday, I will work from home."
+      }
+      return "This Monday, I will go to the office."
+    }
+
+    function wrapAdjective(wrapper) {
+        return function(adjective) {
+          return 'You are ' + wrapper + adjective + wrapper + '!';
+        }
+      }
+      
+  
